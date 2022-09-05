bf85daaa4a019327591a9bde1cb90fa352e19489
rafaelpingolinha <112034828+rafaelpingolinha@users.noreply.github.com>
2022-09-05 19:06:22 -0300

    testegithub

--- bola.py
+++ bola.py
@@ -5,11 +5,13 @@ x = input ("digite o dono da primeira cidade: ")
 
 y = input ("digite o dono da segunda cidade: ")
 
-z = input ("digite o dono da terceira cidade :")
+z = input ("digite o don   o da terceira cidade :")
 
 bola = [x,y,z]
 
 print (bola ) 
+
+print("pinga")
 
 
 
