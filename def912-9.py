8cff80ca317a01e31822d35dc9150e753e13dfcf
rafaelpingolinha <112034828+rafaelpingolinha@users.noreply.github.com>
2022-09-12 20:28:15 -0300

    fiz um trabalho como se fosse uma cauculadora

---
+++ def912-9.py
@@ -0,0 +1,26 @@
+def soma (n1, n2):
+    s = n1 + n2
+    return s
+
+def sub (n1, n2):
+    s = n1 - n2
+    return s
+
+def mult (n1, n2):
+    s = n1 * n2
+    return s
+
+def div (n1, n2):
+    s = n1 / n2
+    return s
+
+print (" escola uma operacao matematica ")
+
+print ( "1 soma " )
+
+print ("2 subtracao")
+
+print ("3 multiplicacao")
+
+print ("4 divisao")
+
